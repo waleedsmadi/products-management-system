6f3d3dd2cb61af2e9f7263581c34148e044d192f
waleed <waleed0smadi@outlook.com>
2022-07-07 09:17:23 +0300

    JS: select elements

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// select elements
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const taxes = document.getElementById("taxes");
+const ads = document.getElementById("ads");
+const discount = document.getElementById("discount");
+const total = document.getElementById("total");
+const count = document.getElementById("count");
+const category = document.getElementById("category");
+const submit = document.getElementById("submit");
